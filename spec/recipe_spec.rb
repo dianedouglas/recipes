a9568d6b9c062d6528c8bbf388d1303e11015adc
Diane Douglas & lee kebler <student@epicodus.com>
2014-08-27 14:34:16 -0700

    Add has and belongs to many relationship between recipes and tags.

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -6,4 +6,10 @@ it {should validate_presence_of :name}
 
 it {should validate_presence_of :description}
 
+  it "should have many tags" do
+    test_recipe = Recipe.create({name: 'Cookies', description: 'Put ingredients in and bake.'})
+    test_tag = Tag.create({name: 'desert'})
+    test_tag.recipes << test_recipe
+    expect(test_tag.recipes).to eq [test_recipe]
+  end
 end
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -4,4 +4,10 @@ describe Tag do
 
   it { should validate_presence_of :name}
 
+  it "should have many recipes" do
+    test_recipe = Recipe.create({name: 'Cookies', description: 'Put ingredients in and bake.'})
+    test_tag = Tag.create({name: 'desert'})
+    test_recipe.tags << test_tag
+    expect(test_recipe.tags).to eq [test_tag]
+  end
 end
